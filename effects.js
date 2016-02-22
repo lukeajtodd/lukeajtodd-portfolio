82cb62a0749978b46c36258cdc3bfd3962d2cba9
lukeajtodd <lukeajtodd@outlook.com>
2016-02-22 10:29:27 +0000

    MOVED FILES INTO DIR

---
+++ effects.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+  var $sparkleDiv = $('.sparkles');
+  var time = new Date().getHours();
+  var elemLimit = 30;
+
+  function checkElemNum() {
+    $sparkCount = $('.spark').length;
+    $starCount = $('.star').length;
+    if ($sparkCount > 0) {
+      if ($sparkCount === elemLimit) {
+        $('.spark:first').remove();
+      }
+    } else {
+      if ($starCount === elemLimit - 5) {
+        $('.star:first').remove();
+      }
+    }
+  }
+
+  function makeElem(elemType) {
+    var maxWidth = $(window).width();
+    var maxHeight = $sparkleDiv.height();
+    checkElemNum();
+    if (elemType == "spark") {
+      var posx = Math.floor(Math.random() * maxWidth);
+      var $newSpark = $("<div class='spark'></div>").css({
+        'left': posx + 'px'
+      });
+      $newSpark.appendTo($sparkleDiv).delay(100).fadeIn(100, function() {
+        makeElem("spark");
+      });
+    } else if (elemType == "star") {
+      var posx = Math.floor(Math.random() * maxWidth);
+      var posy = Math.floor(Math.random() * (maxHeight / 2)) + 20;
+      var $newStar = $("<div class='star'></div>").css({
+        'left': posx + 'px',
+        'top': posy + 'px'
+      });
+      $newStar.appendTo($sparkleDiv).delay(400).fadeIn(100, function() {
+        makeElem("star");
+      });
+    }
+  }
+
+
+  if (time < 7 && time >= 17) {
+    makeElem("star");
+  } else {
+    makeElem("spark");
+  }
+
+
+
+
+
+});
